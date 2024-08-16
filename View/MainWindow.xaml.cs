5da2dc027b0b976620ffddfa913c8a5f094124eb
CassioJhones <cassio.bjhones@gmail.com>
2024-08-16 02:31:42 -0300

    botao para selecionar destino

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -55,5 +55,10 @@ public partial class MainWindow : Window
         => _viewModel.AbrirArquivo(ArquivosListBox);
     private void Click_Cancelar(object sender, RoutedEventArgs e)
         => _viewModel.Cancelar();
+
+    private void Click_SelecionarDestino(object sender, RoutedEventArgs e)
+    {
+
+    }
 }
 
